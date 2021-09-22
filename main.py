8adcc753fa787a1a340f9bf5a31242a4555ed238
bobsini601 <52345499+bobsini601@users.noreply.github.com>
2021-09-23 00:23:31 +0900

    update load_CSV functions
    numpy로 바꾸는 코드 삭제

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-import pandas as pd                     # 데이터를 로드하기 위한 라이브러리
+import pandas as pd  # 데이터를 로드하기 위한 라이브러리
 import numpy as np
-from matplotlib import pyplot as plt    # plotting 하기 위한 라이브러리
-from sklearn import preprocessing       # 데이터를 전처리하기 위한 라이브러리
+from matplotlib import pyplot as plt  # plotting 하기 위한 라이브러리
+from sklearn import preprocessing  # 데이터를 전처리하기 위한 라이브러리
 import sys, os
+
 sys.path.append(os.pardir)
 from sklearn.preprocessing import *
-  
 
-''' csv 파일을 load 해서 numpy 배열로 바꿔주는 함수 '''
-def load_CSV(data):
-    data1 = []
-    data1 = pd.read_csv(data)
-    data2 = data1.to_numpy()
-    return data2
+''' csv 파일을 load해서 반환하는 함수 '''
+def load_CSV(file):
+    data = []
+    data = pd.read_csv(file)
+    return data
 
 ''' 각 파일의 이름 변수에 data 로드 '''
-cus_info=load_CSV('cus_info.csv')              # 고객 및 주거래 계좌 정보
-iem_info=load_CSV('iem_info_20210902.csv')     # 종목 정보 _주식 종목에 대한 코드 정보
-stk_bnc_hist=load_CSV('stk_bnc_hist.csv')      # 국내 주식 잔고 이력 _일별 종목 잔고수량 및 금액, 액면가 정보
-stk_hld_test=load_CSV('stk_hld_test.csv')      # 국내 주식 보유 기간(train) _고객에게 제공되는 과거 국내주식 보유기간 데이터 (681,472건)
-stk_hld_train=load_CSV('stk_hld_train.csv')    # 국내 주식 보유 기간(test) _개발한 알고리즘 검증을 위한 문제지 (70,596건)
+cus_info = load_CSV('cus_info.csv')  # 고객 및 주거래 계좌 정보
+iem_info = load_CSV('iem_info_20210902.csv')  # 종목 정보 _주식 종목에 대한 코드 정보
+stk_bnc_hist = load_CSV('stk_bnc_hist.csv')  # 국내 주식 잔고 이력 _일별 종목 잔고수량 및 금액, 액면가 정보
+stk_hld_test = load_CSV('stk_hld_test.csv')  # 국내 주식 보유 기간(train) _고객에게 제공되는 과거 국내주식 보유기간 데이터 (681,472건)
+stk_hld_train = load_CSV('stk_hld_train.csv')  # 국내 주식 보유 기간(test) _개발한 알고리즘 검증을 위한 문제지 (70,596건)
 
-
-#정규화할 컬럼 추출
+print(stk_bnc_hist.dtypes)
+# 정규화할 컬럼 추출
 bnc_hist_norm = stk_bnc_hist[['bnc_qty', 'tot_aet_amt', 'stk_par_pr']]
 stk_bnc_hist.drop(['bnc_qty', 'tot_aet_amt', 'stk_par_pr'], axis=1, inplace=True)
 
-#데이터 정규화 코드
+# 데이터 정규화 코드
 transformer = MinMaxScaler()
 transformer.fit(bnc_hist_norm)
 bnc_hist_norm = transformer.transform(bnc_hist_norm)
 
 bnc_hist_norm = pd.DataFrame(bnc_hist_norm)
-bnc_hist_norm.columns=(['bnc_qty', 'tot_aet_amt', 'stk_par_pr']) #컬럼에 레이블명 지정
+bnc_hist_norm.columns = (['bnc_qty', 'tot_aet_amt', 'stk_par_pr'])  # 컬럼에 레이블명 지정
 print(bnc_hist_norm)
 
-#정규화한 컬럼을 기존 DataFrame과 수평 결합
+# 정규화한 컬럼을 기존 DataFrame과 수평 결합
 stk_bnc_hist = pd.concat([stk_bnc_hist, bnc_hist_norm], axis=1)
 print(stk_bnc_hist)
